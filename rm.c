494e4f7f2697028249796265951016250e7b431c
ivanderwilliam <ivanderwilliam@gmail.com>
2017-12-06 21:21:52 +0700

    try

--- rm.c
+++ rm.c
@@ -1,1 +1,28 @@
+#include "param.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
 
+int main(int argc, char *argv[])
+{
+  int i;
+
+  if(argc < 2){
+    printf(2, "\n");
+    exit();
+  }
+
+  for(i = 1; i < argc; i++){
+    if(unlink(argv[i]) < 0){
+      printf(2, "rm: %s fail\n", argv[i]);
+      break;
+    }
+  }
+
+  exit();
+}
