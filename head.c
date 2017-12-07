89bfeb637fc273af8cd13e9ba7e11502dc4e0823
irmankurniawan11 <irmankurniawan99@gmail.com>
2017-12-07 14:09:59 +0700

    Fungsi void head

--- head.c
+++ head.c
@@ -2,8 +2,30 @@
 #include "stat.h"
 #include "user.h"
 
+char buf[512];
+
 void head(int fd, char *name, int x) {
+  int n,line=0;
+  int i;
+  
+  while((n = read(fd, buf, sizeof(buf))) > 0 && line<x) {
+    for(i=0; i<=n && line<x; i++) {
+      if(buf[i]!='\n') {
+        printf(1,"%c",buf[i]);
+      }
+      else {
+      	printf(1,"\n");
+      	line++;
+      }
+    }
+  }
+  if(n<0) {
+    printf(1, "HEAD: READ ERROR\n");
+    exit();
+  }
+  printf("file : %s\n", name);
 }
 
 int main(int argc, char *argv[]) {
+  
 }
